87a53de814bcb0b0107c242745365953d08b3aeb
4stron <vkallankari@gmail.com>
2024-10-01 12:10:59 +0300

    button will now tell how many times it has been pressed

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import { useState } from 'react'
 
 function App() {
   const [answer, setAnswer] = useState(null)
-  const [count, setCount] = useState(0)
+  const [requestCount, setRequestCount] = useState(0)
 
   function getData(){
     axios('https://api.scryfall.com/cards/random')
-    .then (resp => setAnswer(resp.data))
+      .then (resp => setAnswer(resp.data))
+      .then(() => setRequestCount(prevCount => prevCount + 1))
   }
 
   return (
     <div>
         <div className='button'>
           <button onClick={getData}>Press for a random card</button>
+          <p>Cards requested: {requestCount}</p>
         </div>
     </div>
   )
