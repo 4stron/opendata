0eb891d6a74d9b817c62454717dd3683d78fb44c
4stron <vkallankari@gmail.com>
2024-10-01 12:06:52 +0300

    created getdata function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 
 function App() {
   const [count, setCount] = useState(0)
+
+  function getData(){
+    axios('https://api.scryfall.com/cards/random')
+    .then (resp => setAnswer(resp.data))
+  }
 
   return (
     <div>
